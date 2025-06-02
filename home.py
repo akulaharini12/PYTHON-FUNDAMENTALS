20ca8e1101020247654e5947955a69380c43516a
akulaharini12 <hariniakula320@gmail.com>
2025-06-02 14:51:47 +0530

    first commit

--- home.py
+++ home.py
@@ -1,1 +1,9 @@
+# print("hello world")
+# x=5
+# print(type(x))
+# # print(len(keyword.kwlist))
+import keyword
+
 print("hello world")
+print(keyword.kwlist)
+print(len(keyword.kwlist))
---
+++ task.py
@@ -0,0 +1,12 @@
+# s=int(input("Enter the number for sumation"))
+# i=0
+# sum=0
+# while i<s: 
+#     sum+=i
+#     i=i+1
+# print("The sumation of the value you have entered is:",sum)    
+
+Radius=int(input("Enter a Radius Value: "))
+pi=3.14
+Area=pi*(Radius**2)
+print(Area)
